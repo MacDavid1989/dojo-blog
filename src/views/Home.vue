887118fcac6e39758bb0c3782e7ec3b3b98f7517
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-25 15:05:03 -0400

    the computed feature is also imported from vue much like refs, we pass in a function to computed properties that return something, the purpose of computed properties is to compute a new value based on other values

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,19 @@
 <template>
   <div>
     <h1>This is the homepage</h1>
-    <h2>Refs</h2>
-    <p ref="p">{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">Update Ninja One</button>
-    <h2>Reactive</h2>
-    <p ref="p">{{ ninjaTwo.name }} - {{ ninjaTwo.age }} - {{ nameTwo }}</p>
-    <button @click="updateNinjaTwo">Update Ninja One</button>
+    <p>{{ name }}</p>
   </div>
 </template>
 
 <script>
-import { reactive, ref } from "@vue/reactivity";
+import { computed, ref } from "@vue/reactivity";
 export default {
   name: "Home",
   setup() {
-    const ninjaOne = ref({ name: "mario", age: 30 });
-    const ninjaTwo = reactive({ name: "luigi", age: 35 });
-    // we can't use primitive values inside reactive as the variable will not become reactive
-    // refs retain their activity when exposed by external functions
-    const nameOne = ref("mario");
-    const nameTwo = reactive("luigi");
-
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40;
-    };
-
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 45;
-      nameTwo = "bowser";
-    };
-
-    return {
-      ninjaOne,
-      updateNinjaOne,
-      ninjaTwo,
-      updateNinjaTwo,
-      nameTwo,
-    };
+    const name = computed(() => {
+      return "shaun";
+    });
+    return { name };
   },
 };
 </script>
