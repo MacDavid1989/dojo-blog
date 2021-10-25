9882d0ddddbb428831028c8da42fb239ac8b556d
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-24 20:07:29 -0400

    refs in the composition api must be imported from vue and returned through setup to connect them to elements, from their we can use refs to perform standard javascript to the element

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,32 @@
 <template>
   <div>
     <h1>This is the homepage</h1>
-    <p>My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick"> Click Me</button>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click Me</button>
   </div>
 </template>
 
 <script>
+import { ref } from "@vue/reactivity";
 export default {
   name: "Home",
   setup() {
+    const p = ref(null);
+
     let name = "mario";
     let age = 30;
 
     const handleClick = () => {
-      console.log('clicked')
+      console.log(p, p.value);
+      p.value.classList.add("test");
+      p.value.textContent = 'Hello Ninjas'
     };
 
     return {
       name,
       age,
-      handleClick
+      handleClick,
+      p,
     };
   },
 };
